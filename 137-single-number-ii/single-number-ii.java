0a43486275cbb70340c13d09037d7221035a3a6f
Deepika-83 <gopanaboyinadeepikayadav@gmail.com>
2026-02-11 22:44:01 +0530

    Time: 60 ms (6.60%) | Memory: 46 MB (30.36%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n=nums.length;
+        int v=0;
+        Arrays.sort(nums);
+        int i=0;
+        while(i<n){ 
+            int c=0;
+            for(int j=0;j<n;j++){
+                    if(nums[i]==nums[j]){
+                        c++;
+                    }
+                
+            }
+            if(c==3){
+                i=i+3;
+            }
+            else if(c<3){
+                v=nums[i];
+             break;
+            }
+            else{
+                i++;
+            }
+        }         
+        return v;
+    }
+}
